644cde873679320edcde5998aab6ea4b64c1d446
Harsh Shah <harshvshah.22@gmail.com>
2024-01-08 21:25:53 -0500

    image saving and posting works

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const imageRoutes = require("./routes/image")
 
 const PORT = process.env.PORT;
+const { CORS_ORIGIN } = process.env;
 app.listen(PORT, () => {
     console.log(`Server Started on http://localhost:${PORT}`);
     console.log("Press CTRL + C to stop server...");
@@ -12,9 +13,9 @@ app.listen(PORT, () => {
 
 app.use(cors({ origin: CORS_ORIGIN }));
 
-app.use(express.json());
-
 app.use('/image-captured', imageRoutes);
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to RxScan API');
--- routes/image.js
+++ routes/image.js
@@ -1,35 +1,32 @@
+const fs = require('fs');
 const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser');
-const multer = require('multer');
 
-// Body-Parser
-router.use(bodyParser.urlencoded({ extended: false }));
-router.use(bodyParser.json());
-
-
-const imageCapPath = '/Users/harshshah/Downloads/rxscanimage';
-
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, imageCapPath)
-    },
-    filename: function(req, file, cb) {
-        cb(null, `${file.fieldname}_dateVal_${Date.now()}_${file.originalname}`)
-    }
-})
+// setting maximum payload size limit
+router.use(bodyParser.json({limit: '50mb'}));
+router.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 
-const imageCap = multer({storage: storage})
 
 // POST requests
-router.post('/image-captured', imageCap.array("image-file"), (req, res) => {
-    console.log('POST request received to /image-captured.')
-    // Adding req.body to test
-    console.log('Axios POST body: ', req.body);
+router.post('/', (req, res) => {
+    const image = req.body.image;
+    console.log(req.body.image);
+    const imageName = `frame_${Date.now()}.jpg`;
+    const imagePath = `/Users/harshshah/Downloads/rxscan-api/images/${imageName}`;
+    const imageBuffer = Buffer.from(image.replace(/^data:image\/\w+;base64,/, ''), 'base64');
 
-    res.send('POST request received on server to /image-captured.')
-})
+    fs.writeFile(imagePath, imageBuffer, (err) => {
+        if (err) {
+          console.error('Error saving image:', err);
+          res.status(500).send('Error saving image');
+        } else {
+          console.log('Image saved:', imageName);
+          res.sendStatus(200);
+        }
+      });
+});
 
 
 module.exports = router;
